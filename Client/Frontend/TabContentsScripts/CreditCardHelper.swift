0a3a3becb4bd25f72fcd96c14bd3f142d4150838
Nishant Bhasin <nish.bhasin@gmail.com>
2023-04-06 00:31:01 -0400

    Refactored script to parse credit card fields from fathom

--- Client/Frontend/TabContentsScripts/CreditCardHelper.swift
+++ Client/Frontend/TabContentsScripts/CreditCardHelper.swift
@@ -6,6 +6,30 @@ import Foundation
 import Shared
 import WebKit
 import Common
+import Storage
+
+enum ValidFieldType: String, CaseIterable {
+    case ccNumber = "cc-number"
+    case ccExpMonth = "cc-exp-month"
+    case ccExpYear = "cc-exp-year"
+    case ccName = "cc-name"
+}
+
+struct Payload: Codable {
+    let id: Int
+    let fieldTypes: [FieldType]
+}
+
+struct FieldType: Codable {
+    let type: String
+    let value: String
+}
+
+struct FillCreditCardForm: Codable {
+    let payload: Payload
+    let type: String
+}
+
 
 class CreditCardHelper: TabContentScript {
     private weak var tab: Tab?
@@ -25,19 +49,49 @@ class CreditCardHelper: TabContentScript {
 
     func userContentController(_ userContentController: WKUserContentController,
                                didReceiveScriptMessage message: WKScriptMessage) {
-        // TODO: FXIOS-6125 - Retrieve response value from user selected credit card
-        guard (message.body as? [String: Any]) != nil else { return }
-        guard message.body is [String: Any] else { return }
-        // TODO: FXIOS-6125 - Check payload received from JS and parse it
-        //
-        // Example value: [
-        //     "data": [
-        //         "cc-name": "Jane Doe",
-        //         "cc-number": "5555555555554444",
-        //         "cc-exp-month": "05",
-        //         "cc-exp-year": "2028",
-        //       ],
-        //     "id": request["id"]!,
-        // ]
+        guard let data = message.body as? [String: Any] else { return }
+        guard let fieldTypes = parseFieldType(messageBody: data) else { return }
+        let fieldTypeValues = getFieldTypeValues(fieldTypes: fieldTypes)
+    }
+
+    func parseFieldType(messageBody: [String: Any]) -> [FieldType]? {
+        let decoder = JSONDecoder()
+
+        do {
+            let jsonData = try JSONSerialization.data(
+                withJSONObject: messageBody, options: .prettyPrinted)
+            let fillCreditCardForm = try decoder.decode(FillCreditCardForm.self,
+                                                        from: jsonData)
+            let fieldTypes = fillCreditCardForm.payload.fieldTypes
+            return fieldTypes
+        } catch {
+            logger.log("Unable to parse field type for CC",
+                       level: .warning,
+                       category: .webview)
+        }
+
+        return nil
+    }
+
+    func getFieldTypeValues(fieldTypes: [FieldType]) -> UnencryptedCreditCardFields {
+        var ccPlainText = UnencryptedCreditCardFields()
+        fieldTypes.forEach { field in
+            if let fieldType = ValidFieldType(rawValue: field.type) {
+                switch fieldType {
+                case .ccName:
+                    ccPlainText.ccName = field.value
+                case .ccExpMonth:
+                    let val = field.value.filter { $0.isNumber }
+                    ccPlainText.ccExpMonth = Int64(val) ?? 0
+                case .ccExpYear:
+                    let val = field.value.filter { $0.isNumber }
+                    ccPlainText.ccExpYear = Int64(val) ?? 0
+                case .ccNumber:
+                    let val = field.value.filter { $0.isNumber }
+                    ccPlainText.ccNumber = "\(val)"
+                }
+            }
+        }
+        return ccPlainText
     }
 }
